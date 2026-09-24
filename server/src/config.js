78f6f7f5ad7357a7dcee90f41ccd0566266f8276
Harsh Upadhyay <harsh9756416665@gmail.com>
2026-09-24 12:43:53 +0530

    feat: add TURN server configuration

--- server/src/config.js
+++ server/src/config.js
@@ -14,10 +14,13 @@ export const config = {
   botMaxRatio: num("BOT_MAX_RATIO", 0.3),
   botLifetimeMinMs: num("BOT_LIFETIME_MIN_MS", 40000),
   botLifetimeMaxMs: num("BOT_LIFETIME_MAX_MS", 110000),
-  msgRateLimit: num("MSG_RATE_LIMIT", 6), // per 10s
+  msgRateLimit: num("MSG_RATE_LIMIT", 6),
   msgRateWindowMs: num("MSG_RATE_WINDOW_MS", 10000),
   skipCooldownMs: num("SKIP_COOLDOWN_MS", 3000),
   banReportThreshold: num("BAN_REPORT_THRESHOLD", 3),
   banDurationMs: num("BAN_DURATION_MS", 24 * 60 * 60 * 1000),
   maxMessageLen: num("MAX_MESSAGE_LEN", 500),
+  turnUrls: (process.env.TURN_URLS || "").split(",").map((v) => v.trim()).filter(Boolean),
+  turnUsername: process.env.TURN_USERNAME || "",
+  turnCredential: process.env.TURN_CREDENTIAL || "",
 };
